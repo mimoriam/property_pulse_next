bc42496836207a918ed29753f27ae12bba6ceca0
Mimo <mimoriam420@gmail.com>
2024-05-09 03:09:31 +0500

    Commit #10 - Added properties in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import InfoBoxes from '@/components/InfoBoxes';
 // @ts-ignore
 import Hero from '@/components/Hero';
+import HomeProperties from '@/components/HomeProperties';
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </div>
   );
 }
--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,10 +1,20 @@
-import Link from 'next/link';
+import PropertyCard from '@/components/PropertyCard';
+import properties from '@/properties.json';
 
 export default function PropertiesPage() {
   return (
-    <div>
-      <h1 className="text-3xl">Properties</h1>
-      <Link href="/">Go Home</Link>
-    </div>
+    <section className="px-4 py-6">
+      <div className="container-xl m-auto px-4 py-6 lg:container">
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
   );
 }
