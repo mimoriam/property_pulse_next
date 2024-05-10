e585761a3487fd2b5ae6b2ef538850f0848c89f4
Mimo <mimoriam420@gmail.com>
2024-05-10 17:01:08 +0500

    Commit #15 - Add API route to fetch all properties

--- app/api/properties/route.jsx
+++ app/api/properties/route.jsx
@@ -1,11 +1,14 @@
 import connectDB from '@/config/database';
 import { NextResponse } from 'next/server';
+import Property from '../../../models/Property';
 
 export const GET = async (request) => {
   try {
     await connectDB();
 
-    return NextResponse.json({ message: 'DB connected' }, { status: 200 });
+    const properties = await Property.find({});
+
+    return NextResponse.json({ properties }, { status: 200 });
   } catch (err) {
     console.log(err);
     return NextResponse.json(
