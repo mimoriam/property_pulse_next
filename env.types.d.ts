d0079434a2c778ece62207639efb987446bd0f4e
Mimo <mimoriam420@gmail.com>
2024-05-10 17:00:34 +0500

    Commit #13 - Update environment variables

--- env.types.d.ts
+++ env.types.d.ts
@@ -1,7 +1,19 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      NEXT_PUBLIC_DOMAIN: string;
+      NEXT_PUBLIC_API_DOMAIN: string;
       MONGODB_URI: string;
+      GOOGLE_CLIENT_ID: string;
+      GOOGLE_CLIENT_SECRET: string;
+      NEXTAUTH_URL: string;
+      NEXTAUTH_URL_INTERNAL: string;
+      NEXTAUTH_SECRET: string;
+      CLOUDINARY_CLOUD_NAME: string;
+      CLOUDINARY_API_KEY: string;
+      CLOUDINARY_API_SECRET: string;
+      NEXT_PUBLIC_MAPBOX_TOKEN: string;
+      NEXT_PUBLIC_GOOGLE_GEOCODING_API_KEY: string;
     }
   }
 }
