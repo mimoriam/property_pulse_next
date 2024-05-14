ac5ce4221ef9c676bd8592f084ccf9f396ad6f67
Mimo <mimoriam420@gmail.com>
2024-05-15 02:32:06 +0500

    Commit #29 - Signify Auth protected, Public and API routes in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,43 @@
-export { auth as middleware } from '@/auth';
+import { NextResponse } from 'next/server';
+import NextAuth from 'next-auth';
+import authConfig from '@/auth.config';
+
+const { auth } = NextAuth(authConfig);
+
+export const authRoutes = ['/properties/add'];
+export const publicRoutes = ['/', '/properties'];
+export const apiAuthPrefix = '/api/auth';
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+
+  if (isApiAuthRoute) {
+    // If routes aren't protected properly, replace this line with below:
+    return NextResponse.next();
+    // return;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return NextResponse.next();
+    }
+    if (!isLoggedIn) {
+      return Response.redirect(new URL('/', nextUrl));
+    }
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL('/', nextUrl));
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
