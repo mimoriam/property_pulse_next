0d9b1ea9960053fca7cdd13047b7e2f0a90c8419
Mimo <mimoriam420@gmail.com>
2024-05-15 02:39:08 +0500

    Commit #31 - Create a add property form

--- app/properties/add/page.tsx
+++ app/properties/add/page.tsx
@@ -1,3 +1,13 @@
+import PropertyAddForm from '@/components/PropertyAddForm';
+
 export default function PropertyAddPage() {
-  return <div>PropertyAddPage</div>;
+  return (
+    <section className="bg-blue-50">
+      <div className="container m-auto max-w-2xl py-24">
+        <div className="m-4 rounded-md border bg-white px-6 py-8 shadow-md md:m-0">
+          <PropertyAddForm />
+        </div>
+      </div>
+    </section>
+  );
 }
