459ebea51f931d217341e823b611f0ae374e3492
Mimo <mimoriam420@gmail.com>
2024-05-20 02:59:01 +0500

    Commit #39 - Delete properties

--- app/api/properties/[id]/route.tsx
+++ app/api/properties/[id]/route.tsx
@@ -1,6 +1,7 @@
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
 import { NextRequest, NextResponse } from 'next/server';
+import { getUserSession } from '@/utils/getUserSession';
 
 // GET /api/properties/:id
 export const GET = async (
@@ -32,6 +33,55 @@ export const GET = async (
     console.error(err);
     return NextResponse.json(
       { error: 'Something went wrong' },
+      { status: 500 },
+    );
+  }
+};
+
+// DELETE /api/properties/:id
+export const DELETE = async (request, { params }) => {
+  try {
+    const propertyId = params.id;
+
+    const sessionUser = await getUserSession();
+
+    // Check for session
+    if (!sessionUser || !sessionUser.user.id) {
+      return NextResponse.json(
+        { message: 'User ID is required' },
+        { status: 401 },
+      );
+    }
+
+    const { id } = sessionUser.user;
+
+    await connectDB();
+
+    const property = await Property.findById(propertyId);
+
+    if (!property)
+      return NextResponse.json(
+        { message: 'Property Not Found' },
+        { status: 404 },
+      );
+
+    // Verify ownership
+    if (property.owner.toString() !== id) {
+      return NextResponse.json('Unauthorized', { status: 401 });
+    }
+
+    await property.deleteOne();
+
+    return NextResponse.json(
+      { message: 'Property Deleted' },
+      {
+        status: 200,
+      },
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { message: 'Something Went Wrong' },
       { status: 500 },
     );
   }
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -24,7 +24,9 @@ const ProfilePage = () => {
       }
 
       try {
-        const res = await fetch(`/api/properties/user/${userId}`);
+        const res = await fetch(`/api/properties/user/${userId}`, {
+          cache: 'no-store',
+        });
 
         if (res.status === 200) {
           const data = await res.json();
@@ -43,7 +45,35 @@ const ProfilePage = () => {
     }
   }, [session]);
 
-  const handleDeleteProperty = () => {};
+  const handleDeleteProperty = async (propertyId) => {
+    const confirmed = window.confirm(
+      'Are you sure you want to delete this property?',
+    );
+
+    if (!confirmed) return;
+
+    try {
+      const res = await fetch(`/api/properties/${propertyId}`, {
+        method: 'DELETE',
+      });
+
+      if (res.status === 200) {
+        // Remove the property from state
+        const updatedProperties = properties.filter(
+          (property) => property._id !== propertyId,
+        );
+
+        setProperties(updatedProperties);
+
+        alert('Property Deleted');
+      } else {
+        alert('Failed to delete property');
+      }
+    } catch (error) {
+      console.log(error);
+      alert('Failed to delete property');
+    }
+  };
 
   return (
     <section className="bg-blue-50">
